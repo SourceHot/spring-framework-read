a7e4c45a121ffd9887be456e0a18615120da90b4
huifer <huifer97@163.com>
2020-05-05 13:52:42 +0800

    dependency

--- spring-source-hot/src/main/java/org/source/hot/spring/overview/ioc/dependency/lookup/Demo.java
+++ spring-source-hot/src/main/java/org/source/hot/spring/overview/ioc/dependency/lookup/Demo.java
@@ -2,6 +2,9 @@ package org.source.hot.spring.overview.ioc.dependency.lookup;
 
 import org.source.hot.spring.overview.ioc.domain.UserEntity;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Demo {
 	public static void main(String[] args) {
 		BeanFactory beanFactory = new ClassPathXmlApplicationContext("META-INF/dependency.xml");
