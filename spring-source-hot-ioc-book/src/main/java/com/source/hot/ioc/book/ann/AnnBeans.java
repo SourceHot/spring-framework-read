e7e92ac8a539d718ca5faa0b11be823a35cb5e3c
huifer <huifer97@163.com>
2021-02-04 16:18:11 +0800

    fix: 测试用例

--- spring-source-hot-ioc-book/src/main/java/com/source/hot/ioc/book/ann/AnnBeans.java
+++ spring-source-hot-ioc-book/src/main/java/com/source/hot/ioc/book/ann/AnnBeans.java
@@ -1,14 +1,20 @@
 package com.source.hot.ioc.book.ann;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope
 public class AnnBeans {
-	@Bean
+	@Bean(name = "abc")
 	public AnnPeople annPeople() {
 		AnnPeople annPeople = new AnnPeople();
 		annPeople.setName("people");
 		return annPeople;
+	}
+
+	class InnerClass {
+
 	}
 }
--- spring-source-hot-ioc-book/src/test/java/com/source/hot/ioc/book/ioc/MetadataReaderFactoryTest.java
+++ spring-source-hot-ioc-book/src/test/java/com/source/hot/ioc/book/ioc/MetadataReaderFactoryTest.java
@@ -5,16 +5,40 @@ import java.io.IOException;
 import com.source.hot.ioc.book.ann.AnnBeans;
 import org.junit.jupiter.api.Test;
 
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.type.classreading.MetadataReader;
 import org.springframework.core.type.classreading.MetadataReaderFactory;
 import org.springframework.core.type.classreading.SimpleMetadataReaderFactory;
+import org.springframework.util.ClassUtils;
 
 public class MetadataReaderFactoryTest {
 
 	@Test
 	void testSimpleMetadataReaderFactory() throws IOException {
 		MetadataReaderFactory factory = new SimpleMetadataReaderFactory();
+		MetadataReader metadataReader = factory.getMetadataReader(MetadataReaderFactoryTest.class.getName());
+		System.out.println();
+	}
+
+	@Test
+	void testInnerName() {
+		String className = MetadataReaderFactoryTest.A.class.getName();
+		int lastDotIndex = className.lastIndexOf('.');
+		String innerClassName =
+				className.substring(0, lastDotIndex) + '$' + className.substring(lastDotIndex + 1);
+		String innerClassResourcePath = ResourceLoader.CLASSPATH_URL_PREFIX +
+				ClassUtils.convertClassNameToResourcePath(innerClassName) + ClassUtils.CLASS_FILE_SUFFIX;
+		System.out.println(innerClassResourcePath);
+	}
+
+	@Test
+	void me() throws IOException {
+		MetadataReaderFactory factory = new SimpleMetadataReaderFactory();
 		MetadataReader metadataReader = factory.getMetadataReader(AnnBeans.class.getName());
 		System.out.println();
+	}
+
+	class A {
+
 	}
 }
