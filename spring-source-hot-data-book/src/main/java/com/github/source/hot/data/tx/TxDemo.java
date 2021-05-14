d76c81c12177b6aa4b796a928934a51a3049666b
huifer <huifer97@163.com>
2021-05-14 17:06:23 +0800

    fix: tx

--- spring-source-hot-data-book/src/main/java/com/github/source/hot/data/tx/TxDemo.java
+++ spring-source-hot-data-book/src/main/java/com/github/source/hot/data/tx/TxDemo.java
@@ -1,9 +1,13 @@
 package com.github.source.hot.data.tx;
 
+import org.springframework.cglib.core.DebuggingClassWriter;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TxDemo {
 	public static void main(String[] args) {
+//		System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
+		System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY,
+				"D:\\desktop\\git_repo\\spring-ebk\\spring-framework-read\\spring-source-hot-data-book\\prox");
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-tx-01.xml");
 		WorkService bean = context.getBean(WorkService.class);
 		bean.work();
